de1685c09e8759087eb0f498996274f1ffc7d78d
jaime <garciadiazjaime@gmail.com>
2021-12-09 14:23:02 -0600

    chore: adjust post limit

--- module/image/cron-entry.js
+++ module/image/cron-entry.js
@@ -15,7 +15,7 @@ async function main() {
   }
 
   let count = 0;
-  await mapSeries(posts.slice(0, 1), async (post) => {
+  await mapSeries(posts.slice(0, 30), async (post) => {
     const { id, mediaUrl } = post;
 
     count += 1;
