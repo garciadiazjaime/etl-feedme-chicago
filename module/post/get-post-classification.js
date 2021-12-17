c89c0ee67d37cc5d10a91f85451cba4ed38960ad
jaime <garciadiazjaime@gmail.com>
2021-12-17 17:22:30 -0600

    feat: classification route

---
+++ module/post/get-post-classification.js
@@ -0,0 +1,49 @@
+const { PostModel } = require('./model');
+
+async function getPostClassification(limit = 10000) {
+  const posts = await PostModel.find({
+    $and: [
+      {
+        classification: {
+          $ne: null,
+        },
+      },
+      {
+        classification: {
+          $ne: [],
+        },
+      },
+    ]
+    ,
+  }, {
+    _id: false,
+    classification: true,
+  })
+    .sort({ createdAt: -1 })
+    .limit(limit);
+
+  const classification = posts.reduce((accu, post) => {
+    post.classification.forEach((item) => {
+      if (!accu[item.className]) {
+        accu[item.className] = 0; //eslint-disable-line
+      }
+
+      accu[item.className] += 1; //eslint-disable-line
+    });
+
+    return accu;
+  }, {});
+
+  const sorted = Object.keys(classification)
+    .map((className) => ({
+      className,
+      total: classification[className],
+    }))
+    .sort((a, b) => b.total - a.total);
+
+  const threshold = Math.round(sorted[0].total * 0.01);
+
+  return sorted.filter((item) => item.total > threshold);
+}
+
+module.exports = getPostClassification;
--- module/post/routes.js
+++ module/post/routes.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const getPostsByDay = require('./get-posts-by-day');
 const getPostsSummary = require('./get-posts-summary');
 const getPost = require('./get-posts');
+const getPostClassification = require('./get-post-classification');
 
 const router = express.Router();
 
@@ -23,6 +24,12 @@ router.get('/posts', cors(), async (req, res) => {
   const posts = await getPost();
 
   res.send(posts);
+});
+
+router.get('/posts/classification', cors(), async (req, res) => {
+  const classifications = await getPostClassification();
+
+  res.send(classifications);
 });
 
 module.exports = router;
